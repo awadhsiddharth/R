5da4e1d418d4bfc34e05402b8e0346e2b1bb3713
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2025-10-13 13:29:28 +0100

    Add files via upload

---
+++ DSA-1(Vector).R
@@ -0,0 +1,33 @@
+# Data Structure - Built in data struture
+# Vector, Matrix, Array, List, Dataframe, Factor
+
+
+# Vector - 1 D collection of data(homogenous in nature)
+x <- c(2,3,452,133,53,"hi")
+x - charcater
+typeof(x)
+x <- c(1,1,1,1,12)
+x
+typeof(x)
+
+
+x <- c(seq(1,20,2))
+x
+# Indexing
+x[2]
+x[8]
+x[16]
+length(x)
+x[10]
+
+
+# x[-1] except 1st all element is shown
+x[-2]
+
+# Slicing
+x[1:4]
+
+x[1,5,8] # wrong syntax
+x[c(1,5,9)]
+x[-c(1,5,9)]
+
